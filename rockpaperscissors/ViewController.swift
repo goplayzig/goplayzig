3ae0068545bb57befe31897346da2e2ae000a3c4
Playzig <clueplayzig@gmail.com>
2016-11-13 01:56:23 +0900

    2016/11/13 1:56 commit

--- rockpaperscissors/ViewController.swift
+++ rockpaperscissors/ViewController.swift
@@ -7,13 +7,66 @@
 //
 
 import UIKit
+import GameplayKit
 
 class ViewController: UIViewController {
-
+    let randomSource = GKARC4RandomSource()
+    
+    @IBOutlet weak var playerImageView: UIImageView!
+    @IBOutlet weak var computerImageView: UIImageView!
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let angle:CGFloat = CGFloat((180.0 * M_PI) / 180.0)
+        
+        computerImageView.transform = CGAffineTransform(rotationAngle : angle)
     }
+    
+    
+    
+    
+    @IBAction func tabStart() {
+        
+        computerImageView.image = UIImage(named: "rock.png")
+        playerImageView.image = UIImage(named: "rock.png")
+        messageLabel.text = "가위 바위 ..."
+    }
+    
+    @IBAction func tapScissors() {
+        playerImageView.image = UIImage(named:"siccors.png")
+    }
+    @IBAction func tapPaper() {
+        playerImageView.image = UIImage(named:"paper.png")
+        
+    }
+    
+    @IBAction func tabRock() {
+        playerImageView.image = UIImage(named : "rock.png")
+    }
+
+    func doComputer() {
+        
+        let computer = randomSource.nextInt(upperBound:3)
+        switch computer {
+        case 0 :
+            
+            computerImageView.image = UIImage(named:"siccors.png")
+            
+        case 1 :
+            computerImageView.image = UIImage(named: "rock.png")
+            
+        case 2 :
+            computerImageView.image = UIImage(named: "paper.png")
+            
+        default:
+            break
+        }
+        
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -21,5 +74,6 @@ class ViewController: UIViewController {
     }
 
 
-}
 
+
+}
